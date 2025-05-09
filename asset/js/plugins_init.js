184273684d3647df63cbcb61ffa2a79e2d07b8dd
Cassidyou <cassidyclever@gmail.com>
2025-05-09 16:43:50 +0100

    Update after review on 8th March 2025

--- asset/js/plugins_init.js
+++ asset/js/plugins_init.js
@@ -282,20 +282,20 @@ $(document).ready(function () {
 
   /****************** Select2 *************************************/
   // Basic initialization
-  $('.select2-select-search').select2();
+  // $('.select2-select-search').select2();
 
   // Optional: Add a placeholder
-  $('.select2-select-search').select2({
-    placeholder: "Select",
-    allowClear: !true
-  });
+  // $('.select2-select-search').select2({
+  //   placeholder: "Select",
+  //   allowClear: !true
+  // });
 
   
   /****************** Dropify *************************************/
-  if ($('.dropify').length > 0) {
-    $('.dropify').dropify({
+  if ($('.dropifye').length > 0) {
+    $('.dropifyes').dropify({
       messages: {
-        'default': 'Drag and drop your photos here',
+        'default': 'Drag and drop your file here',
         'replace': 'Drag and drop or click to replace',
         'remove': 'Remove',
         'error': 'Ooops, something wrong appended.'
@@ -304,7 +304,7 @@ $(document).ready(function () {
         'fileSize': 'The file size is too big (2M max).'
       },
       tpl: {
-        message: '<div class="dropify-message"><span class="file-icon" /> <p style="font-size: 18px!important; color: #000000; opacity: 0.7;"><b>{{ default }}</b></p><small style="font-size: 14px">Choose at least 5 photos</small><br> <span style="border-bottom: 1px solid; font-size: 14px; color: #000; margin-bottom: 5px;opacity: 0.7;">Upload from device</span></div>',
+        message: '<div class="dropify-message"><span class="file-icon" /> <p style="font-size: 18px!important; color: #000000; opacity: 0.7;"><b>{{ default }}</b></p><small style="font-size: 14px">Accepted formats: png, jpg</small><br> <span style="border-bottom: 1px solid; font-size: 14px; color: #000; margin-bottom: 5px;opacity: 0.7;">Upload from device</span></div>',
 
       }
     });
